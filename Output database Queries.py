62116e72732c62e6a1557ab8ea25c9b486ae305d
Alex <darkblueshadow66@googlemail.com>
2019-05-07 20:23:41 +0200

    Completed First Tree Structure

--- Output database Queries.py
+++ Output database Queries.py
@@ -2,12 +2,17 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+
 import DB_Test as db  # import from the file with the database query
 
 d = dash
-app = d.Dash()
+app = d.Dash(
+    external_stylesheets=['layout.css'],
+    external_scripts=['Zdropdown.js'])
 
-app.layout = html.Div(children=[
+# app.css.append_css()
+
+app.layout = html.Div([
     html.H1(
         children="let's do this"
     ),
@@ -26,9 +31,68 @@ app.layout = html.Div(children=[
                 yaxis={'title': 'amount of patients'}
             )
         }
+    ),
+    html.Ul(
+        [
+            html.Span(
+                className='caret',
+                children='Beverages'
+            ),
+            html.Ul(
+                [
+                    html.Div('Coffee'),
+                    html.Div('Water'),
+                    html.Li(
+                        [
+                            html.Span(
+                                'Tea',
+                                className='caret',
+                            ),
+                            html.Ul(
+                                [
+                                    html.Div('White Tea'),
+                                    html.Div('Black Tea'),
+                                    html.Li(
+                                        [
+                                            html.Span(
+                                                'Green Tea',
+                                                className='caret'
+
+                                            ),
+
+                                            html.Ul(
+                                                [
+                                                    html.Div('Sencha'),
+                                                    html.Div('Spi Lon Chun'),
+                                                    html.Div('Gyokuro'),
+                                                    html.Div('Matcha'),
+                                                ],
+
+                                                className='nested'
+
+                                            )
+                                        ]
+                                    )
+                                ],
+                                className='nested',
+
+                            )
+                        ]
+                    )
+                ],
+                className='nested',
+            )
+        ],
+        id='tree_view_test',
+    ),
+    html.Link(
+        href='Zdropdown.css',
+        rel='stylesheet'
     )
-]
+],
+    # gdc.Import(src="Zdropdown.js")
 )
 
 if __name__ == '__main__':
+    # app.layout += []
     app.run_server(debug=True)
