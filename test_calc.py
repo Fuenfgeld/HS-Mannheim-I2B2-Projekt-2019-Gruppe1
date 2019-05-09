f9684cacba03a2d5bf8bbcc8173e876190a158b2
RicardoBrauckmann <1722355@stud.hs-mannheim.de>
2019-05-09 13:35:47 +0200

    test_calc.py verändert

--- test_calc.py
+++ test_calc.py
@@ -4,5 +4,6 @@ import calc as c #zu testende files importen
 class TestCalc(unittest.TestCase): #unittest.TestCase muss übergeben werden
 
     def test_add(self): #muss mit test_ anfangen! Dann Name der zu testenden Methode
-        result = c.add(10,5)
-        self.assertEqual(result, 15)
+        self.assertEqual(c.add(10,5), 15)
+        self.assertEqual(c.add(-1, 1), 0)
+        self.assertEqual(c.add(-1, -1), -2)
