c8163cf0b7cf88262903eaa42e4c6286107c4e3c
surfsidepalmy <sven@janssen-online.eu>
2019-05-13 15:49:59 +0200

    Stand 13.5.2019

---
+++ FormattedSQL.py
@@ -0,0 +1,18 @@
+def SQLStatement(StringA, StringB, StringD):
+    SQL1 = f'select * from {StringA} where {StringB}={StringD}'
+    return SQL1
+
+
+def SQLJoin(String1, String2, String3, String4, String5, String6, String7):
+    SQL1 = f'select * from {String1} ' \
+        f'join {String2} on {String1}.{String3}= {String2}.{String3}' \
+        f' where {String4}={String5} and {String6}={String7}'
+    return SQL1
+
+
+def SQLBack(String1, String2, String3, String4, String5, String6, String7, String8):
+    SQL1 = f'select {String6}.{String1} from {String2} ' \
+        f'join {String3} on {String3}.{String4} = {String2}.{String5} ' \
+        f'join {String6} on {String2}.{String1} = {String6}.{String1} ' \
+        f'where {String3}.{String7} like {String8}'
+    return SQL1
--- app.py
+++ app.py
@@ -2,14 +2,54 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
+import sqlalchemy as sa
+import pandas as pd
+
+import FormattedSQL as fs
+import StringSQL as s
+
 import database as db
+engine = sa.create_engine("postgresql://i2b2:demouser@129.206.7.75:5432/i2b2")
+
+sql1 = pd.read_sql("SELECT * FROM i2b2.i2b2metadata.icd10_icd9 WHERE c_hlevel = 1;", con=engine)
+i = len(sql1)
+print(i)
+
+sql2 = pd.read_sql("select * from i2b2.i2b2demodata.patient_dimension where language_cd like 'eng%%'", con=engine)
+a = len(sql2)
+print(a)
+
+sql3 = pd.read_sql("select distinct i2b2.i2b2demodata.patient_dimension.patient_num from i2b2.i2b2demodata.observation_fact "
+                   "join i2b2.i2b2metadata.icd10_icd9 on i2b2.i2b2metadata.icd10_icd9.c_basecode = i2b2.i2b2demodata.observation_fact.concept_cd "
+                   "join i2b2.i2b2demodata.patient_dimension on i2b2.i2b2demodata.observation_fact.patient_num = i2b2.i2b2demodata.patient_dimension.patient_num "
+                   "where i2b2.i2b2metadata.icd10_icd9.c_tooltip like 'Diagnoses \ Diseases of the circulatory system%%'", con=engine)
+e = len(sql3)
+print(e)
+
+#curser = db.conn.cursor()
+#curser.execute(fs.SQLJoin(s.String1, s.String2, s.String3, s.String4, s.String5, s.String6, s.String7))
+#resulta = curser.rowcount
+#curser.close()
+
+#curser1 = db.conn.cursor()
+#curser1.execute(
+#    fs.SQLBack(s.String11, s.String22, s.String33, s.String44, s.String55, s.String66, s.String77, s.String88))
+#resultb = curser1.rowcount
+#curser1.close()
+
+#print(resultb)
+#print(resultb)
 
 app = dash.Dash(external_stylesheets=['style.css'])
-
 app.layout = html.Div([
-    html.Div([html.H1("Explorer")], className="Div1"),
-    html.Div([html.H1("Ergebnisse")], className="Div2"),
-    html.Div([html.H1("Query-Leiste")], className="Div3")
+#    html.Div([html.H1("Explorer")], className="Div1"),
+#    html.Div([html.H1(resultb)], className="Div2"),
+#    html.Div([
+#        html.Div([html.H1(s.StringC)], className="StringC"),
+#        html.Div(
+#            [html.Div([html.H1(s.String5)], className="String5"), html.Div([html.H1(s.String7)], className="String7")],
+#            className="String5_7")
+#    ], className="Div3")
 ])
 
 # Stop Flask Server Strg+C
