cdded4589bb84c7211148eb4f7e6611532c726d7
RicardoBrauckmann <1722355@stud.hs-mannheim.de>
2019-05-15 11:05:33 +0200

    sqlTemplate erweitert: anzahlPatZweiKriterienAND und ...OR hinzugefuegt

--- abfragenLogik/sqlTemplates.py
+++ abfragenLogik/sqlTemplates.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 engine = sa.create_engine("postgresql://i2b2:demouser@129.206.7.75:5432/i2b2")
 
-def patEinKriteriumBlatt(name_char):
+def patProKriteriumBlatt(name_char):
     df = pd.read_sql(f'select c_basecode from i2b2.i2b2metadata.i2b2 where c_name = \'{name_char}\'', con = engine)
     s = "ICD9"
     for n in range(0, 10):
@@ -13,15 +13,15 @@ def patEinKriteriumBlatt(name_char):
     df2 = pd.read_sql(f'select distinct patient_num from i2b2.i2b2demodata.observation_fact where concept_cd like \'{icd + "%%"}\'', con = engine)
     return df2
 
-def anzahlPatEinKriteriumBlatt(name_char):
-    return len(patEinKriteriumBlatt(name_char))
+def anzahlPatProKriteriumBlatt(name_char):
+    return len(patProKriteriumBlatt(name_char))
 
 def anzahlPatEinKriteriumEltern():
     return 0
 
 def anzahlPatZweiKriterienAND(name_char1, name_char2):
-    df1 = patEinKriteriumBlatt(name_char1)
-    df2 = patEinKriteriumBlatt(name_char2)
+    df1 = patProKriteriumBlatt(name_char1)
+    df2 = patProKriteriumBlatt(name_char2)
     j = 0
     for n in range(0, len(df1)):
         m = df1.loc[n].values[0]
@@ -30,5 +30,13 @@ def anzahlPatZweiKriterienAND(name_char1, name_char2):
                 j += 1
     return j
 
-def anzahlPatZweiKriterienOR():
-    return 0
+def anzahlPatZweiKriterienOR(name_char1, name_char2):
+    df1 = patProKriteriumBlatt(name_char1)
+    df2 = patProKriteriumBlatt(name_char2)
+    j = len(df1) + len(df2)
+    for n in range(0, len(df1)):
+        m = df1.loc[n].values[0]
+        for i in range(0, len(df2)):
+            if m == df2.loc[i].values[0]:
+                j -= 1
+    return j
--- abfragenLogik/test.py
+++ abfragenLogik/test.py
@@ -3,8 +3,10 @@ import abfragenLogik.sqlTemplates as st
 
 #print(sg.gesamtanzahlPatienten())
 try:
-    print(st.anzahlPatEinKriteriumBlatt("Hypertensive renal disease"))
+    print(st.anzahlPatProKriteriumBlatt("Hypertensive renal disease"))
 except:
     print('Eltern')
 
 print(st.anzahlPatZweiKriterienAND("Essential hypertension", "Hypertensive renal disease"))
+
+print(st.anzahlPatZweiKriterienOR("Essential hypertension", "Hypertensive renal disease"))
