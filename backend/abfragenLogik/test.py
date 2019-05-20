4782ec32e36167ef8a53b7b6908bd175aa6c3283
RicardoBrauckmann <1722355@stud.hs-mannheim.de>
2019-05-20 14:59:28 +0200

    test hinzugefügt

--- backend/abfragenLogik/test.py
+++ backend/abfragenLogik/test.py
@@ -3,21 +3,8 @@ from backend.abfragenLogik import sql_templates as sql_templates
 from backend.abfragenLogik import sql_verknuepfungen as sql_verknuepfungen
 
 gesamtAnzahl = gesamt_anzahl.GesamtAnzahl()
+sqlTemplates = sql_templates.SQLTemplates()
 sqlVerknuepfung = sql_verknuepfungen.SQLVerknuepfung()
-
-# print(sg.gesamtanzahlPatienten())
-# try:
-#     print(st.anzahlPatProKriteriumBlatt("Hypertensive renal disease"))
-# except:
-#     print('Eltern')
-
-
-sT = sqlTemplates.sqlTemplates()
-sG = sqlGesamtanzahl.sqlGesamtanzahl()
-
-print(sT.anzahlPatZweiKriterienAND("Essential hypertension", "Hypertensive renal disease"))
-
-print(sT.anzahlPatZweiKriterienOR("Essential hypertension", "Hypertensive renal disease"))
 
 # Ein Callback ist nichts anderes als ein Zeiger auf eine Funktion. Also eine Funktion, in der die Adresse einer anderen Funktion gespeichert wird.
 funclist = [gesamtAnzahl.gesamtanzahlPatienten(), gesamtAnzahl.gesamtanzahlMaenner(), gesamtAnzahl.gesamtanzahlFrauen()]
@@ -32,16 +19,13 @@ try:
     # print(gesamtAnzahl.gesamtanzahlFrauen())
 
     # rufe über import auf
-    print(len(sql_templates.pat_df_ein_kriterium_blatt_cd('Essential hypertension')))
-    print(len(sql_templates.pat_df_ein_kriterium_blatt_i2b2('Essential hypertension')))
+    print(len(sqlTemplates.pat_df_ein_kriterium_blatt_i2b2('Essential hypertension')))
 
     # 1 Patient
     # print(sqlVerknüpfung.anzahlPatZweiKriterienAND('Essential hypertension', 'Hypertensive renal disease'))
     # 40 Patienten
     # print(sqlVerknüpfung.anzahlPatZweiKriterienOR('Essential hypertension', 'Hypertensive renal disease'))
 
-    # Versuch Eltern mit allen Unterknoten ausgeben
-    # print(st.anzahlPatEinKriteriumEltern(c_fullname))
 except:
     print('Eltern')
 
