9c64ae6c8f3610fd5ad35def64c0e4f4234a94fe
Alex <darkblueshadow66@googlemail.com>
2019-05-29 20:01:23 +0200

    Working whole version (render time: 1 hour) with 10 tests, tree will be converted to dicionary and then locally saved as pickle thus enhancing accessing speed by a lot

--- level_dictionary(faster).py
+++ level_dictionary(faster).py
@@ -25,39 +25,39 @@ def add_first_level(metaPath):
     print(RenderTree(rootNode))  # show the tree using the tree renderer
 
 
-def add_next_level(level, metaNodeList, metaPathList):
+def add_next_level(level, parentNodeList, parentPathList):
     print('adding level: ', level)  # used for clarification which level is added
 
     levelQuery = db.Queries()  # new Object is created
     wholeLevelList = levelQuery.get_level(level)  # Database query put into a data frame
+    levelFullNames = wholeLevelList['fullname']  # full paths of the first level (former c_fullpath)
     levelCopy = wholeLevelList.copy(deep=True)  # as the original list is changed
     # an untouched deep copy is needed (rename variable)
-    levelFullNames = wholeLevelList['fullname']  # full paths of the first level (former c_fullpath)
     copyFullNames = levelCopy['fullname']
     levelNames = wholeLevelList['name']
     levelPaths = wholeLevelList['path']
     metaListIndex = 0  # index of the list of the parent nodes (rename variable)
     wholeLvelIndex = 0  # index of the whole list of the level (rename variable)
 
-    # print('Full name: ', levelFullNames[0])
-    # print('Path: ', levelPaths[0])
-    # print('meta path: ', metaPathList[0])
-    # print('Name: ', levelNames[0])
-    # print('Parent: ', metaNodeList[0])
-
-    while wholeLvelIndex <= len(wholeLevelList) and metaListIndex < len(metaNodeList):  # while both indices
+    while wholeLvelIndex < len(wholeLevelList) and metaListIndex <= len(parentNodeList):  # while both indices
         # are in range
 
-        if levelPaths[wholeLvelIndex] == metaPathList[metaListIndex]:  # if path of child aligns with the fullpath of
+        if levelPaths[wholeLvelIndex] == parentPathList[metaListIndex] or metaListIndex == len(parentNodeList):
+            # if path of child aligns with the fullpath of
             # the (parent) node in the list, this node in the list is the parent of the child and gets appended to it
             # print('True')
-            levelFullNames[wholeLvelIndex] = Node(f'{levelNames[wholeLvelIndex]}', parent=metaNodeList[metaListIndex])
+            levelFullNames[wholeLvelIndex] = Node(f'{levelNames[wholeLvelIndex]}', parent=parentNodeList[metaListIndex])
             wholeLvelIndex += 1
         else:  # if paths do not align, the next parent node is looked on
             # print('Next one')
             metaListIndex += 1
 
-    if level <= 4:  # there is a limit to the levels, without this statement this would create an endless loop
+    if level <= 9:  # there is a limit to the levels, without this statement this would create an endless loop
+        print('Length of the whole level list: ', len(wholeLevelList))
+        print('Index of the whole list: ', wholeLvelIndex)
+        print('Length of parent list: ', len(parentNodeList))
+        print('Parent list index: ', metaListIndex)
+        print('')
         add_next_level(level + 1, levelFullNames, copyFullNames)
 
 
