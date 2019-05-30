13bd1a79eec29f8dc0da3407c1335a406529c8a6
RicardoBrauckmann <1722355@stud.hs-mannheim.de>
2019-05-30 20:29:45 +0200

    aenderung in graph_language_distribution

--- backend/result_logic/graph_language_distribution.py
+++ backend/result_logic/graph_language_distribution.py
@@ -12,24 +12,39 @@ class graphLanguageDistribution:
 
     def set_layout_language_graph(self, df):
         dfNew = result_merge.merge_two_df(df, 'language_cd')
-        count_english = dfNew.language_cd.str.count('english').sum()
-        count_spanish = dfNew.language_cd.str.count('spanish').sum()
-        count_german = dfNew.language_cd.str.count('german').sum()
+        count_english_new = dfNew.language_cd.str.count('english').sum()
+        count_spanish_new = dfNew.language_cd.str.count('spanish').sum()
+        count_german_new = dfNew.language_cd.str.count('german').sum()
+
+        dfOverAll = result_merge.build_df_language_cd_patient_dimension()
+        count_english_over_all = dfOverAll.language_cd.str.count('english').sum()
+        count_spanish_over_all = dfOverAll.language_cd.str.count('spanish').sum()
+        count_german_over_all = dfOverAll.language_cd.str.count('german').sum()
+
 
         layoutLanguageDistribution = html.Div([
 
             dcc.Graph(
                 id='language_distribution',
                 figure=go.Figure(
-                    data=[go.Bar(x=['Englisch', 'Spanisch', 'Deutsch'],
-                                 y=[count_english, count_spanish, count_german],
-                                 marker=dict(color=['#248f24', '#47d147','#99e699'],
-                                             line=dict(color='#a3a3c2', width=2)),
-                                 )
+                    data=[
+                        go.Bar(x=['Englisch', 'Spanisch', 'Deutsch'],
+                               y=[count_english_over_all, count_spanish_over_all, count_german_over_all],
+                               marker=dict(color=['#9494b8', '#9494b8','#9494b8'],
+                                           line=dict(color='#a3a3c2', width=2)),
+                               name='Grundgesamtheit',
+                              ),
+                        go.Bar(x=['Englisch', 'Spanisch', 'Deutsch'],
+                               y=[count_english_new, count_spanish_new, count_german_new],
+                               marker=dict(color=['#248f24', '#47d147','#99e699'],
+                                           line=dict(color='#a3a3c2', width=2)),
+                               name='Ausgewählte Kohorte',
+                              )
                           ],
 
                     layout=go.Layout(
                         title='Verteilung nach Muttersprache',
+                        showlegend=True,
 
                     )
                 )
