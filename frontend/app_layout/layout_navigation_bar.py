423fb2141772971c696956f923462abedcbba85a
RicardoBrauckmann <1722355@stud.hs-mannheim.de>
2019-06-05 14:53:15 +0200

    Kleine Aenderung in app.py, layout_navigation_bar; #10 #13 #17 #47

--- frontend/app_layout/layout_navigation_bar.py
+++ frontend/app_layout/layout_navigation_bar.py
@@ -5,7 +5,7 @@ import dash_html_components as html
 class layoutNavigationBar:
 
     layout_navigation = html.Div([
-        dcc.Input(id='input-box', placeholder='Suche', type='text', className="DivSuchen"),
+        dcc.Input(id='input-box', placeholder='Eingabe', type='text', className="DivSuchen"),
         html.Button(id='add-button', children='Add', n_clicks=0),
         html.Div(id='clicked-button', children='del:0 add:0 last:nan', style={'display': 'none'}),
         html.Div(
