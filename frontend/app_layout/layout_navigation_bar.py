f8d0529a8e8fcca77c9058c7bed2cf163130300c
Benjamin Tuna <1624691@stud.hs-mannheim.de>
2019-06-13 12:20:03 +0200

    Layout angepasst #47 #19 #37 #15

--- frontend/app_layout/layout_navigation_bar.py
+++ frontend/app_layout/layout_navigation_bar.py
@@ -4,9 +4,16 @@ import dash_html_components as html
 
 class layoutNavigationBar:
 
+    layoutSearchBar = html.Div([
+        dcc.Input(id='input-box', placeholder='Eingabe', type='text', className='DivSuchen'),
+        html.Button(id='add-button', children='Add', n_clicks=0, className='DivAdden')
+
+    ], className="DivSearch")
+
+
+
+
     layout_navigation = html.Div([
-        dcc.Input(id='input-box', placeholder='Eingabe', type='text', className="DivSuchen"),
-        html.Button(id='add-button', children='Add', n_clicks=0),
         html.Div(id='clicked-button', children='del:0 add:0 co1:0 last:nan', style={'display': 'none'}),
         html.Div(
             [html.Div(className='container'),
@@ -14,4 +21,4 @@ class layoutNavigationBar:
              html.Div(id='jstree-result')]
         )
 
-    ], className="DivNavigation")
+    ], className="DivTree")
